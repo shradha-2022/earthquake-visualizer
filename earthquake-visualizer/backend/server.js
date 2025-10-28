194556eb98bacf999c6f1ee92a71b863bc4bb9f9
Shradha-2022 <91013212+shradha-2022@users.noreply.github.com>
2025-10-28 19:42:17 +0530

    Update server.js

--- earthquake-visualizer/backend/server.js
+++ earthquake-visualizer/backend/server.js
@@ -21,6 +21,7 @@ app.get("/api/earthquakes", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`✅ Backend running on port ${PORT}`);
+  console.log(`✅ Server is running on http://0.0.0.0:${PORT}`);
 });
